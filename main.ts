58105212214f919a4f37e98ab757c421f83cf23b
jaromir <jaromirblu@gmail.com>
2022-08-11 11:14:12 +0200

    basic mysql connection

---
+++ main.ts
@@ -0,0 +1,42 @@
+import { Client } from 'https://deno.land/x/mysql/mod.ts';
+import { Application, Context, Router } from 'https://deno.land/x/oak/mod.ts';
+import { connect_mysql } from "./mysql.ts";
+
+const port = 8000;
+const app = new Application();
+const router = new Router();
+
+
+// handle mysql queries from client
+router.post("/mysql", async (ctx: Context) => {
+    const body = await ctx.request.body({type: "json"}).value; // {conn, query}
+
+    const conn_details = body["conn"]; // {id, name, database, host, user, type, password}
+    const query: string = body["query"]; // some string
+
+    try {
+        // connect to mysql database
+        const client: Client = await connect_mysql(conn_details);
+
+        // run query
+        const exec_result = await client.execute(query);
+
+        // return results
+        ctx.response.status = 200;
+        ctx.response.body = exec_result;
+
+    } catch (error: any) {
+        const err = error as Error;
+        console.log(err.message);
+        ctx.response.status = 401;
+    }
+});
+
+
+// handle postgresql queries from client
+// ...
+
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.listen({port});
---
+++ mysql.ts
@@ -0,0 +1,16 @@
+import { Client } from "https://deno.land/x/mysql/mod.ts";
+
+
+// connect to mysql database and return client
+export const connect_mysql = async (conn_details: any): Promise<Client> => {
+
+    const client = new Client();
+    await client.connect({
+        hostname: conn_details["host"],
+        username: conn_details["user"],
+        password: conn_details["password"],
+        db: conn_details["database"]
+    });
+
+    return client;
+};
